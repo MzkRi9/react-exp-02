f518a276ae6a9c7ffeec46dffb21fe14ffa9baee
MzkRi9 <cidy416@gmail.com>
2026-01-18 13:24:59 +0900

    授業の更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import News from './components/News/index.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [箱の名前, 動作：箱のuseStateを設定する(原則setXxxxx、Xxxxxxは箱の名前)] = useState(useStateの初期値)
+  // inputが2つあるので、入力したものを保持するためにuseStateを2つ用意する
+  const [name, setName] = useState('名前入力')
+  const [email, setEmail] = useState('メールアドレス入力')
+
+  // イベント処理（何をしたら何をする）
+
+  const handleNameChange = (e) => {
+    // 処理を書きます
+    setName (e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail (e.target.value);
+  }
+
+  // useEffectの前に追加でコンソールログを書く
+  console.log('順番の確認しよう');
+
+  useEffect(() => {
+    // この中に書きます
+    console.log('起動しました！');
+  }, [])
 
   return (
     <>
+     {/*  */}
+     <News />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>名前が入ります</p>
+        {/* inputに必ずvalue属性を設定する。そしてuseStateと紐づく */}
+        <input type="text" placeholder='名前を入力してください' value={name} onChange={handleNameChange}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <p>メールアドレスが入ります</p>
+        <input type="text" placeholder='メールアドレスを入力してください' value={email} onChange={handleEmailChange}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+     {/*  */}
     </>
   )
 }
