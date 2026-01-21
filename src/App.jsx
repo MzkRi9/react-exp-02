e5e438391426287123634eaac5943a148f7bc301
MzkRi9 <cidy416@gmail.com>
2026-01-21 20:14:58 +0900

    課題の更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,22 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import News from './components/News/index.jsx'
+import Chart from "./pages/chart";
+import Home from "./pages/home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/notfound";
 
 function App() {
-  // const [箱の名前, 動作：箱のuseStateを設定する(原則setXxxxx、Xxxxxxは箱の名前)] = useState(useStateの初期値)
-  // inputが2つあるので、入力したものを保持するためにuseStateを2つ用意する
-  const [name, setName] = useState('名前入力')
-  const [email, setEmail] = useState('メールアドレス入力')
-
-  // イベント処理（何をしたら何をする）
-
-  const handleNameChange = (e) => {
-    // 処理を書きます
-    setName (e.target.value);
-  }
-
-  const handleEmailChange = (e) => {
-    setEmail (e.target.value);
-  }
-
-  // useEffectの前に追加でコンソールログを書く
-  console.log('順番の確認しよう');
-
-  useEffect(() => {
-    // この中に書きます
-    console.log('起動しました！');
-  }, [])
-
   return (
     <>
-     {/*  */}
-     <News />
-      <div>
-        <p>名前が入ります</p>
-        {/* inputに必ずvalue属性を設定する。そしてuseStateと紐づく */}
-        <input type="text" placeholder='名前を入力してください' value={name} onChange={handleNameChange}/>
-      </div>
-      <div>
-        <p>メールアドレスが入ります</p>
-        <input type="text" placeholder='メールアドレスを入力してください' value={email} onChange={handleEmailChange}/>
-      </div>
-
-     {/*  */}
+      {/*  */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chart" element={<Chart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      {/*  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
